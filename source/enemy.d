b548b2784d5cc2251d4051396b47f5c4708bb6a6
Enalye <enalyen@gmail.com>
2018-07-13 23:37:34 +0200

    Added epoch

--- source/enemy.d
+++ source/enemy.d
@@ -8,6 +8,7 @@ import th.grid;
 import th.entity;
 import th.input;
 import th.shot;
+import th.game;
 
 class Enemy: Entity {
     private {
@@ -29,16 +30,17 @@ class Enemy: Entity {
     //Called when the player is acting
     void action() {
         _direction = cast(Direction)(uniform!"[]"(cast(int)Direction.NONE, cast(int)Direction.FIRE_RIGHT));
-        import std.stdio;
-        writeln(_direction);
+        if(!checkDirectionValid(_direction)) {
+            _lastDirection = _direction;
+            return;
+        }
         if(isMovement(_direction)) {
             _lastDirection = _direction;
             currentGrid.set(Type.None, gridPosition); //when going away reset grid data to none
 
             gridPosition = getUpdatedPosition(_direction);
         }
-
-        if(isFire(_direction)) {
+        else if(isFire(_direction)) {
             _lastDirection = _direction;
             float angle = angleFromFireDirection(_direction);
             createEnemyShot(_position, Color.blue, angle, 5f, 5 * 60f);
