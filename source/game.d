d1d182c316e389b680e5ee76e1963c0d44311b8b
Enalye <enalyen@gmail.com>
2018-07-13 17:27:27 +0200

    Added shots logic

--- source/game.d
+++ source/game.d
@@ -5,6 +5,7 @@ import grimoire;
 import th.camera;
 import th.entity;
 import th.stage;
+import th.shot;
 
 private {
     Scene _scene;
@@ -21,12 +22,15 @@ private final class Scene: Widget {
     private {
         Camera _camera;
         Stage _stage;
+        ShotArray _playerShots, _enemyShots;
     }
 
     this() {
         _position = centerScreen;
 		_size = screenSize;
 		_camera = createCamera(centerScreen, screenSize);
+        _playerShots = createPlayerShotArray();
+        _enemyShots = createEnemyShotArray();
     }
 
     override void onPosition() {
@@ -39,7 +43,28 @@ private final class Scene: Widget {
     
     override void onEvent(Event event) {}
 
-    override void update(float deltaTime) {}
+    override void update(float deltaTime) {
+        //Update shots
+        foreach(Shot shot, uint index; _playerShots) {
+			shot.update(deltaTime);
+			if(!shot.isAlive)
+				_playerShots.markInternalForRemoval(index);
+			//Handle collisions with enemies
+
+		}
+
+        foreach(Shot shot, uint index; _enemyShots) {
+			shot.update(deltaTime);
+			if(!shot.isAlive)
+				_enemyShots.markInternalForRemoval(index);
+			//Handle collisions with the player
+
+		}
+
+        _playerShots.sweepMarkedData();
+        _enemyShots.sweepMarkedData();
+
+    }
 
     override void draw() {
 		pushView(_camera.view, true);
@@ -54,8 +79,8 @@ private final class Scene: Widget {
 
     void onStage1() {
         _stage = new Stage(Vec2u(5, 7));
-        uint playerPoolId = _stage.pools.push(new EntityPool("fairy_default"));
-        uint enemyPoolId = _stage.pools.push(new EntityPool("fairy_default"));
+        uint playerPoolId = _stage.pools.push(new EntityPool);
+        uint enemyPoolId = _stage.pools.push(new EntityPool);
         _stage.addPlayerData(Vec2i(0, 0), playerPoolId);
         _stage.addEnemyData(Vec2i(0, 5), enemyPoolId);
         _stage.addEnemyData(Vec2i(4, 5), enemyPoolId);
