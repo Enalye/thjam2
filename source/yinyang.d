60ec86f05a2c7237901ab93c2230c6be331b8b9b
Jonath Kane <jonathkane@gmail.com>
2018-07-14 15:40:08 +0200

    Solving bug in yinyang.d

--- source/yinyang.d
+++ source/yinyang.d
@@ -7,21 +7,17 @@ import th.input;
 import th.item;
 
 class YinYang: Item {
-	private {
-		Direction _direction;
-	}
-
 	this(Vec2i gridPosition, Direction direction) {
 		super(gridPosition, ItemType.YINYANG);
 		type = Type.Enemy;
 		_direction = direction;
 		_resetDirectionAuto = false;
-		_debug = true;
 		_collectible = false;
 	}
 
 	override void action() {
 		if(isMovement(_direction) && checkDirectionValid(_direction)) {
+			writeln(_direction);
 			moveOnGrid();
 		} else {
 			_direction = getOppositeDirection(_direction);
