9f7b85f5ded4134f8b8836b389c1f8aaae281589
Enalye <enalyen@gmail.com>
2018-07-15 19:08:09 +0200

    added game over

--- source/menu.d
+++ source/menu.d
@@ -11,6 +11,11 @@ void onMainMenu() {
     addWidget(new MainMenu);
 }
 
+void onGameOver() {
+    removeWidgets();
+    addWidget(new GameOverScreen);
+}
+
 final private class MainMenu: Widget {
     private {
         Sprite _titleSprite, _backgroundSprite, _pressSprite, _reimuSprite;
@@ -67,5 +72,65 @@ final private class MainMenu: Widget {
         _reimuSprite.draw(centerScreen);
         _pressSprite.draw(centerScreen);
         _titleSprite.draw(centerScreen);
+    }
+}
+
+final private class GameOverScreen: Widget {
+    private {
+        Sprite _titleSprite, _backgroundSprite, _pressSprite, _reimuSprite, _goSprite;
+        Timer _timer, _startTimer;
+        bool _isStarting;
+    }
+
+    this() {
+        _position = centerScreen;
+        _size = screenSize;
+
+        _titleSprite = fetch!Sprite("title");
+        _backgroundSprite = fetch!Sprite("title_background");
+        _pressSprite = fetch!Sprite("title_press");
+        _reimuSprite = fetch!Sprite("title_reimu");
+        _goSprite = fetch!Sprite("gameover");
+
+        _timer.start(1.5f, TimeMode.Loop);
+    }
+
+    override void onEvent(Event event) {
+
+    }
+
+    override void update(float deltaTime) {
+        if(getKeyDown("start")) {
+            _isStarting = true;
+            _startTimer.start(1.5f);
+            playSound(SoundType.Start);
+        }
+        _startTimer.update(deltaTime);
+        
+        if(_isStarting && !_startTimer.isRunning) {
+            startGame();
+        }
+        else if(_isStarting) {
+            _timer.update(deltaTime * 5f);
+        }
+
+        _timer.update(deltaTime);
+    }
+
+    override void draw() {
+        if(_timer.time < .5f) {
+            float t = _timer.time * 2f;
+            _pressSprite.color = lerp(Color.white, Color(1f, 1f, 1f, .25f), 1f - easeOutSine(t));
+        }
+        else {
+            float t = (_timer.time - .5f) * 2f;
+            _pressSprite.color = lerp(Color.white, Color(1f, 1f, 1f, .25f), easeInSine(t));
+        }
+
+        _backgroundSprite.draw(centerScreen);
+        _reimuSprite.draw(centerScreen);
+        _pressSprite.draw(centerScreen);
+        _titleSprite.draw(centerScreen);
+        _goSprite.draw(centerScreen);
     }
 }
